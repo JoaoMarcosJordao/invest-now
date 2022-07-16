199fc6ea3d3e54e8f7fabf30f5cfb9e0ab10c516
joaomarcos <joaomarc97@gmail.com>
2022-07-16 18:34:30 -0300

    feat: feita toda a funcionalidade da página de login, input para email e senha, e botao para acessar a proxima pagina. caso o email nao esteja no formato xxx@xx.com gera um erro, caso senha nao tenho pelo menos 8 caracters, ira gerar um erro. e caso os dois estejam errados tbm irá gerar um erro. feito o login é redirecionado para a pag de acoes com a url /actions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Actions from "./pages/Actions";
 import Login from './pages/Login'
 
 function App() {
   return (
     <>
-    <Login />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/actions" component={Actions} />
+      </Switch>
     </>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
-
+import { Redirect } from 'react-router-dom';
 import emailValidation from '../utils/emailValidation';
 
 function Login() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const [invalid, setInvalid] = useState({});
 
   const handleChangeEmail = ({ value }) => {
+    const date = new Date();
 
     const emailValidated = emailValidation(value);
 
     if (emailValidated) {
+
+      localStorage.setItem('RegistrationDate', date)
       localStorage.setItem('userEmail', value)
 
       setEmail(value);
@@ -23,50 +28,84 @@ function Login() {
     setPassword(value)
   }
 
+
   const acessButtonClick = () => {
 
     const validEmail = emailValidation(email);
 
     if (!validEmail && password.length < 8) {
-      return alert('email e senha invalidos');
+      return setInvalid({
+        emailPassword: true
+      });
     }
 
-    if (!validEmail) return alert('email invalido');
+    if (!validEmail) {
+      return setInvalid({
+        email: true
+      });
+    };
 
-    if (password.length < 8) return alert('senha invalida');
+    if (password.length < 8) {
+      return setInvalid({
+        password: true
+      });
+    };
+
+    setRedirect(true);
 
   }
 
   return (
     <section>
       <div>
-        <label htmlFor='input-email'>
-            <input
-              name='input-email'
-              placeholder='Email'
-              type='text'
-              data-testid='input-email'
-              onChange={ ({ target }) => handleChangeEmail(target) }
-            />
-        </label>
-      </div>
-      <div>
-        <label htmlFor='input-password'>
+        <div>
+          <input
+            name='input-email'
+            placeholder='Email'
+            type='text'
+            data-testid='input-email'
+            onChange={({ target }) => handleChangeEmail(target)}
+          />
+
+          {
+            invalid.email && <span className='invalid'>*Email inválido*</span>
+          }
+
+        </div>
+
+        <div>
           <input
             name='input-password'
             placeholder='Senha'
             type='password'
             data-testid='input-password'
-            onChange={ ({ target }) => handleChangePassword(target) }
+            onChange={({ target }) => handleChangePassword(target)}
           />
-        </label>
-      </div>
 
-      <button
-        onClick={() => acessButtonClick()}
-      >
-        Acessar
-      </button>
+          {
+            invalid.password && <span className='invalid'>*Senha inválida*</span>
+          }
+
+        </div>
+
+        <div>
+
+          {
+            invalid.emailPassword && <span className='invalid'>*Email e senha inválidos*</span>
+          }
+
+          <button
+            onClick={() => acessButtonClick()}
+          >
+            Acessar
+          </button>
+        </div>
+
+        {
+          redirect && <Redirect push to='/actions' />
+        }
+
+      </div>
     </section>
   );
 }
